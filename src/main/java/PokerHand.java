e761255c3b11322e80d089840faedc3f13de6d6e
MarcusC67 <93521177+MarcusC67@users.noreply.github.com>
2022-04-24 20:56:03 +0100

    Green - initial commit first test checkStripFiveCardsFromWhite()

--- src/main/java/PokerHand.java
+++ src/main/java/PokerHand.java
@@ -11,12 +11,27 @@ public class PokerHand {
         // Strip player name "Black" from input string
         int pos1 = input.indexOf(':');
         String name1 = input.substring(0, pos1);
-        
+
         // Strip player name "White" from input string
         int pos2 = input.indexOf(':', pos1 + 1);
         String name2 = input.substring((pos2-5), pos2);
 
-        return name1;
+        // Strip out each of the 5 cards for player named Black
+        String hand1 = input.substring(pos1+2, pos2-7);
+        String[] parts1 = hand1.trim().split(" +"); // split on any single or multiple space
+
+        // Strip out each of the 5 cards for player named White
+        int len = input.length();
+        String hand2 = input.substring(pos2+2, len);
+        String[] parts2 = hand2.trim().split(" +");
+
+        for (int i=0; i < 5; i++) {
+            System.out.println("parts 2 array = " + parts2[i]);
+        }
+
+        return Arrays.toString(parts2).replace("[", "").replace("]", "").replace(",", "");
 
     }
+
+
 }
--- src/test/java/PokerHandTest.java
+++ src/test/java/PokerHandTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -5,11 +6,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PokerHandTest {
 
-        @ParameterizedTest
-        @CsvSource({"Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH, Black"})
-        void checkStripNameBlack(String input, String expected) {
-            assertEquals(expected, PokerHand.stripInput(input));
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({"Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH, Black"})
+    void checkStripNameBlack(String input, String expected) {
+        assertEquals(expected, PokerHand.stripInput(input));
+    }
 
-        }
+    @ParameterizedTest
+    @CsvSource({"Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH, 2C 3H 4S 8C AH"})
+    void checkStripFiveCardsFromWhite(String input, String expected) {
+        assertEquals(expected, PokerHand.stripInput(input));
+    }
 
 }
